a0038e5c72a2e16357b09ff80370d2816caf3791
Dinshahnazaryan <dinshahnazaryan@gmail.com>
2025-04-11 22:30:20 +0400

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
 
 android {
     namespace = ("com.example.mathquiz")
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = ("com.example.mathquiz")
-        minSdk = 21
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = ("1.0")
@@ -27,7 +27,15 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.credentials:credentials:1.2.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.0")
+    implementation("androidx.annotation:annotation-experimental:1.4.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
-    implementation("com.google.firebase:firebase-firestore:25.0.0")
-    implementation("com.google.android.gms:play-services-base:18.5.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.firebase.firestore)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
