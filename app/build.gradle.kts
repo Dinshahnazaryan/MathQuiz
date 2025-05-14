ef9d6232e35c5ea84055fc993e3f97e7e7aa0cb0
Dinshahnazaryan <dinshahnazaryan@gmail.com>
2025-05-14 19:39:15 +0400

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,23 +36,17 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-inappmessaging")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-livedata:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.8.6")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:core:1.6.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
-    configurations.all {
-        resolutionStrategy {
-            force("com.google.firebase:firebase-common:32.7.0")
-        }
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
-
 pluginManagement {
     repositories {
         google()
-        gradlePluginPortal()
         mavenCentral()
-    }
-    plugins {
-        id("com.android.application") version "8.2.2"
-        id("com.google.gms.google-services") version "4.4.2"
+        gradlePluginPortal()
     }
 }
-
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 rootProject.name = "MathQuiz"
 include(":app")
-
